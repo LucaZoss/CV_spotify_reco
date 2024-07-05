12c722f33a9d4003cc6a354136fcdcb51544690d
LucaZoss <lucazosso@gmail.com>
2024-07-05 09:28:55 +0200

    FINAL VERSION (debugging)

--- face_recognition.py
+++ face_recognition.py
@@ -3,24 +3,27 @@ from keras.models import model_from_json
 import numpy as np
 import os
 
+
 class FaceRecognition:
     def __init__(self, json_file_path, weights_file_path):
         # Load the model architecture
         with open(json_file_path, "r") as json_file:
             model_json = json_file.read()
         self.model = model_from_json(model_json)
-        
+
         # Check if the weights file exists and load the weights
         if os.path.exists(weights_file_path):
             self.model.load_weights(weights_file_path)
         else:
             raise FileNotFoundError("The weights file does not exist.")
-        
+
         # Load pre-trained face detection model
-        self.face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
-        
+        self.face_cascade = cv2.CascadeClassifier(
+            cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+
         # Define labels for emotion classes
-        self.labels = {0: 'angry', 1: 'disgust', 2: 'fear', 3: 'happy', 4: 'neutral', 5: 'sad', 6: 'surprise'}
+        self.labels = {0: 'angry', 1: 'disgust', 2: 'fear',
+                       3: 'happy', 4: 'neutral', 5: 'sad', 6: 'surprise'}
 
     def extract_features(self, image):
         image = image / 255.0
